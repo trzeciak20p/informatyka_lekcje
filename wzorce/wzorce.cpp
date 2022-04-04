7844c2655ed06c5569586a14c98cd2774d56d76c
trzeciak20p <m.trzeciak.p20@gmail.com>
2022-04-04 22:19:39 +0200

    ae

--- wzorce/wzorce.cpp
+++ wzorce/wzorce.cpp
@@ -41,51 +41,77 @@ int zad1(){
     return wynik;
 }
 
-// int zad2(){
-//     fstream plik;
-//     plik.open("wzorce.txt", ios::in);
+bool zad2plsHelp(string linia, string nowy_wzorzec, int x){
+
+
+}
+
+bool czyPusty(string co){
+    for(int i = 0; i < co.length(); i++){
+        cout << "Is this even necesery?" << endl;
+        
+
+    }
+}
+
+int zad2(){
+    fstream plik;
+    plik.open("wzorce.txt", ios::in);
     
-//     int wynik = 0;
-//     int nr_linii = 0;
-//     string wzorzec, linia;
+    int wynik = 0;
+    int nr_linii = 0;
+    string wzorzec, linia;
 
-//     while(getline(plik, linia)){
-//         int ile_znakow = 0; 
-//         string nowy_wzorzec;
+    while(getline(plik, linia)){
+        int ile_znakow = 0; 
+        string nowy_wzorzec;
 
-//         if(nr_linii == 0){
-//             wzorzec = linia;
-//         }else{
-//             for(int i = 0; i < linia.length(); i++){
-//                 bool popsuta_linia = false;
-//                 for(int j = 0; j < wzorzec.length(); j++){
-//                     if(linia[i] == wzorzec[j]){
-//                         if(linia[i] == nowy_wzorzec[j]){
-//                             ile_znakow++;
-//                             nowy_wzorzec[j] = NULL;
-//                             popsuta_linia = false;
-//                         }else{
-//                             popsuta_linia = true;
-//                         }                       
-//                     }
-//                 }
-//                 bool popsuty_wrzozec = false;
-//                 for(int n = 0; n < nowy_wzorzec.length(); n++){
-//                      if(nowy_wzorzec[n] != NULL){
-//                         popsuty_wzorzec = true;
-//                      }
-//                 }
+        if(nr_linii == 0){
+            wzorzec = linia;
+        }else{            
+            for(int i = 0; i < linia.length(); i++){
+
+
+            }
+            if(nowy_wzorzec == NULL){
+                wynik++ ;
+            }
+
+
+
+
+
+
+            // for(int i = 0; i < linia.length(); i++){
+            //     bool popsuta_linia = false;
+            //     for(int j = 0; j < wzorzec.length(); j++){
+            //         if(linia[i] == wzorzec[j]){
+            //             if(linia[i] == nowy_wzorzec[j]){
+            //                 ile_znakow++;
+            //                 nowy_wzorzec[j] = NULL;
+            //                 popsuta_linia = false;
+            //             }else{
+            //                 popsuta_linia = true;
+            //             }                       
+            //         }
+            //     }
+            //     bool popsuty_wrzozec = false;
+            //     for(int n = 0; n < nowy_wzorzec.length(); n++){
+            //          if(nowy_wzorzec[n] != NULL){
+            //             popsuty_wzorzec = true;
+            //          }
+            //     }
                 
-//                 if(!popsuta_linia && !popsuty_wzorzec){
-//                     wynik++ ;
-//                 }
-//             }
-//         }
-//         nr_linii++ ; 
-//     }
+            //     if(!popsuta_linia && !popsuty_wzorzec){
+            //         wynik++ ;
+            //     }
+            }
+        }
+        nr_linii++ ; 
+    }
 
-//     return wynik;
-// }
+    return wynik;
+}
 
 
 bool czyTeSame(string linia, string wzorzec, int x){
