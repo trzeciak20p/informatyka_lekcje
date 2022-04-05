ead6a673a9d0b55e9e29ec0bda5337b12602f579
trzeciak20p <91263438+trzeciak20p@users.noreply.github.com>
2022-04-05 14:17:01 +0200

    Add files via upload

--- ip/ip.cpp
+++ ip/ip.cpp
@@ -2,23 +2,54 @@
 
 using namespace std;
 
+struct IP{
+    string Ip;
+    int bin;
+    int bint[4];
+};
 
-bool czyIpZgodneZMaska(string ip, string maska){
-    
-
+bool czyLiczbyMaSame(string ip){
+    for(int i = 0; i < ip.length(); i++){
+        if(ip[i] < 48 && ip[i] > 57 || ip[i] != '.'){        //sprawdza czy ma same cyfry lub kropke
+            return false;
+        }
+    }
+    return true;
 }
 
-int wczytywanieIp(){
+bool czyIpZgodne(string ip, string maska){
+    if(!czyLiczbyMaSame){
+        return false;
+    }
+
+
+    else return true;
+}
+
+IP wczytywanieIp(){
+    // IP yo;
     string ip, maska;
+    
     do{
         cout << "Podaj adres IP: ";
         cin >> ip;
         cout << "Podaj maskę: ";
         cin >> maska;
-    }while(!czyIpZgodneZMaska);
+    }while(!czyIpZgodne);
 
 
-    return ip;
+    // yo -> ip = ip;
+    // yo -> maska = maska;
+
+    // for(int i = 0; i < ip.length(); i++ ){
+
+
+    // }
+    
+
+    
+
+    return yo;
 }
 
 int main(int argc, char const *argv[])
