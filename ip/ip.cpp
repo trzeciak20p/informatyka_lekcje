46eec739432f6be3b8f527f57160810c0d08e6fb
trzeciak20p <91263438+trzeciak20p@users.noreply.github.com>
2022-04-26 14:05:38 +0200

    Add files via upload

--- ip/ip.cpp
+++ ip/ip.cpp
@@ -2,33 +2,124 @@
 
 using namespace std;
 
+// Wczytać adres IP i maskę 
+// Maska Czy 4 oktety czy od zera do 255 i czy ciągłość same jedynki potem same zera 
+// 1. Wyświetlić adres IP i maskę w binarce (każdy oktet na bin i połączyć)
+// 2. Wyświetlić klasę IP i ilość podejść dla klasy maski (eee maska jedynki -  IP jedynki cośtam)
+// 3. Wyświetlić adres sieci, ilość hostów w podsieci
+// 4. sprawdzić poprawność maski oraz IP ()
+
 struct IP{
-    string Ip;
-    int bin;
-    int bint[4];
+    string ip;
+    string mask;
+    int IP_bin[4];
+    int mask_bin[4];
 };
 
-bool czyLiczbyMaSame(string ip){
-    for(int i = 0; i < ip.length(); i++){
-        if( (ip[i] < '0' && ip[i] > '9') || ip[i] != '.'){        //sprawdza czy ma same cyfry lub kropke
-            return false;
+bool czyLiczbyMaSame(IP x){     //works
+    for(int i = 0; i < x.ip.length(); i++){
+        if( x.ip[i] < '0' || x.ip[i] > '9'){        //sprawdza czy ma same cyfry lub kropke
+            if(x.ip[i] != '.'){
+                cout << x.ip[i] << endl;
+                cout << "żle cyferki i kropki" << endl;
+                return false;
+            }
         }
     }
-    cout << "Git cyferki" << endl;
+    for(int i = 0; i < x.mask.length(); i++){
+        if( x.mask[i] < '0' || x.mask[i] > '9'){        
+            if(x.mask[i] != '.'){
+                cout << "żle cyferki i kropki" << endl;
+                return false;
+            }
+        }
+    }
+
+    cout << "Git cyferki i kropki" << endl;
     return true;
 }
 
-bool czyIpZgodne(string ip, string maska){
-    if(!czyLiczbyMaSame(ip)){
+bool naPoprawnoscBin(IP x){
+    
+    
+}
+
+bool czySkladniaDobrze(IP x){       //works
+    int ae = 0;             //ilośc numerków między kropkami
+    int ile_kropek = 0;
+    for(int i = 0; i < x.ip.length(); i++){
+        if(x.ip[i] != '.'){
+            ae++ ;   
+            if(ae == 4){
+                cout << "Źle składnia" << endl;
+                return false;
+            } 
+        }else{
+            if(ae == 0){
+                cout << "Brak znaków między kropkami" << endl;
+                return false;
+            }
+            ae = 0;
+            ile_kropek++ ;
+            if(ile_kropek == 4){               
+                cout << "Źle składnia (za dużo kropek)" << endl;
+                return false;
+            }
+        }        
+    }
+    if(ile_kropek < 3 || ae == 0){
+        cout << "Źle składnia" << endl;
+        return false;
+    }
+    //maska
+    ile_kropek = 0;
+    ae = 0;
+    for(int i = 0; i < x.mask.length(); i++){
+        if(x.mask[i] != '.'){
+            ae++ ;   
+            if(ae == 4){
+                cout << "Źle składnia" << endl;
+                return false;
+            }
+        }else{
+            if(ae == 0){
+                cout << "Brak znaków między kropkami" << endl;
+                return false;
+            }
+            ae = 0;
+            ile_kropek++ ;
+            if(ile_kropek == 4){
+                cout << "Źle składnia (za dużo kropek)" << endl;
+                return false;
+            }
+        }      
+    }
+    if(ile_kropek < 3 || ae == 0){
+        cout << "Źle składnia" << endl;
+        return false;
+    }
+
+    cout << "Git składnia";
+    return true;
+}
+
+bool czyIpZgodne(IP x){
+    if(!czyLiczbyMaSame(x)){
+        return false;
+    }
+    if(!czySkladniaDobrze(x)){
+        return false;
+    }
+    if(!naPoprawnoscBin(x)){
         return false;
     }
 
 
-    else return true;
+    return true;
 }
 
 void wczytywanieIp(){
-    // IP yo;
+    IP ajpi;
     string ip, maska;
 
     do{
@@ -36,16 +127,15 @@ void wczytywanieIp(){
         cin >> ip;
         cout << "Podaj maskę: ";
         cin >> maska;
-    }while(!czyIpZgodne(ip, maska));
+        ajpi.ip = ip;
+        ajpi.mask = maska;
+    }while(!czyIpZgodne(ajpi));
 
 
-    // yo -> ip = ip;
-    // yo -> maska = maska;
-
-    // for(int i = 0; i < ip.length(); i++ ){
+    for(int i = 0; i < ip.length(); i++ ){
 
 
-    // }
+    }
     
 
     
