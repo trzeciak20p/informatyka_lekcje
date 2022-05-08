5bad01044946d89690cb604f0fe17d558344fe2f
trzeciak20p <m.trzeciak.p20@gmail.com>
2022-05-08 23:21:30 +0200

    ae

--- objektowe/index.php
+++ objektowe/index.php
@@ -7,7 +7,7 @@ class Student_public{
     public string $klasa;
     public int $nr_w_dzienniku;
 
-    public function _constructor(string $imie, string $nazwisko, string $klasa, string $nr_w_dzienniku){
+    public function __construct(string $imie, string $nazwisko, string $klasa, int $nr_w_dzienniku){
         $this -> imie = $imie;
         $this -> nazwisko = $nazwisko;
         $this -> klasa = $klasa;
@@ -16,29 +16,32 @@ class Student_public{
 }
 
 $student = new Student_public("Jan", "Kowalski", "2I", 37);
-$student2 = new Student_public();
+$student2 = new Student_public("Stefan", "Batory", "3I", 12);
 $student2 -> imie = "Jacek";
 $student2 -> nazwisko = "Placek";
 $student2 -> nazwisko = "Kowal";
 $student2 -> klasa = "2B";
 
-echo "$student -> imie $student -> nazwisko"; 
+echo $student -> imie . " ". $student -> nazwisko ; 
 
+echo "<br><br><br>";
 
 class Student_php8{
+
     public function __construct(
         public string $imie,
         public string $nazwisko,
         public string $klasa,
-        public int $nr_w_dzienniku
+        public int $nr_w_dzienniku,
     ) {}
+    
 }
 
-$student3 = new Student_php8("Marcin", "Kowalski", "3A", 45)
+$student3 = new Student_php8("Marcin", "Kowalski", "3A", 45);
 $student3 -> imie = "Zenek";
-echo "$student -> imie $student -> nazwisko klasa: $student3 -> klasa"; 
+echo $student -> imie ." ". $student -> nazwisko ." klasa: ". $student3 -> klasa; 
 
-
+echo "<br><br><br>";
 
 class Student_private{
 
@@ -61,7 +64,7 @@ class Student_private{
         $this -> nr_w_dzienniku = $nr_w_dzienniku;
     }
     public function displayAll(){
-        echo "Imie: $this -> imie Nazwisko: $this -> nazwisko Klasa: $this -> klasa Numer w dzienniku: $this -> nr_w_dzienniku: "
+        echo "Imie: ". $this -> imie .", nazwisko: ". $this -> nazwisko .", klasa: ". $this -> klasa .", numer w dzienniku: ". $this -> nr_w_dzienniku ;
     }
 
     public function setName(string $imie){
