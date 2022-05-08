885f7aded251ccc5375857333d8eb529b8e4a791
trzeciak20p <m.trzeciak.p20@gmail.com>
2022-05-08 22:52:43 +0200

    ae

--- objektowe/index.php
+++ objektowe/index.php
@@ -24,4 +24,25 @@ class Student_php8{
     ) {}
 }
 
+
+class Student_private{
+
+    private string $imie;
+    private string $nazwisko;
+    private string $klasa;
+    private int $nr_w_dzienniku;
+
+    public function setAll(string $imie, string $nazwisko, string $klasa, string $nr_w_dzienniku){
+        $this -> imie = $imie;
+        $this -> nazwisko = $nazwisko;
+        $this -> klasa = $klasa;
+        $this -> nr_w_dzienniku = $nr_w_dzienniku;
+    }
+    public function displayAll(){
+        echo "Imie: $this -> imie Nazwisko: $this -> nazwisko Klasa: $this -> klasa Numer w dzienniku: $this -> nr_w_dzienniku: "
+    }
+
+}
+
+
 ?>
